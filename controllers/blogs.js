45f2c4282e802fa75af262c75cfe9eb1820c5915
Padmanabhan <dev.pdmnhn@gmail.com>
2022-01-27 01:37:32 +0530

    exercises 4.13 - 4.14 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,14 +6,31 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => response.status(400).end());
+  try {
+    const result = await blog.save();
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+  await Blog.findByIdAndDelete(id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const id = request.params.id;
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  };
+  const result = await Blog.findByIdAndUpdate(id, blog, { new: true });
+  response.status(200).json(result);
 });
 
 module.exports = blogsRouter;

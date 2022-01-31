f7ca3d4f75fe85ef62c80d8493c0fc01700d2bac
Padmanabhan <dev.pdmnhn@gmail.com>
2022-02-01 00:30:36 +0530

    didn't add the untracked files to the last commit

---
+++ .REST Client/login_token.js
@@ -0,0 +1,6 @@
+const obj = {
+  username: "metaverse",
+  name: "full stack developer",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1ldGF2ZXJzZSIsImlkIjoiNjFmODEyMjEwODg1MjY5YWFiNWMyMjAwIiwiaWF0IjoxNjQzNjQ3NjAwfQ.l4nc36sAaLdV-wVmrbzV2rl9BjZNf2zntUAuqqtDIoI",
+};
